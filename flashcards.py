fff46ef892d1a3e2e71bf3fa1b5f4ed4750158c2
kallesillanpaa <96225525+kallesillanpaa@users.noreply.github.com>
2022-02-14 15:03:12 +0200

    Add files via upload
    Flashcards 14.2.2022 tilanne klo 15

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify, request
-from model import load_db
+from flask import Flask, render_template, abort, jsonify, request, url_for,redirect
+from model import load_db, save_db
 
 app = Flask(__name__)
 
@@ -30,13 +30,23 @@ def add_card():
                 "answer": request.form['answer']
                }
         db.append(card)
-        return render_template("welcome.html", cards=db)        
+        save_db(db)
+        return redirect(url_for('card_view', index=len(db)-1))
     else: #menee GET:iin
         return render_template("add_card.html")
 
 
-
-
+@app.route("/delete_card/<int:index>", methods=["GET","POST"])
+def delete_card(index):
+    try:
+        if request.method=="POST":
+            del db[index]
+            save_db(db)
+            return redirect(url_for('welcome'))
+        else:
+            return render_template("delete_card.html", card=db[index], index=index)
+    except IndexError:
+        abort(404)
 
 #REST API
 @app.route("/api/card")
--- model.py
+++ model.py
@@ -4,3 +4,6 @@ def load_db():
     with open("db.json") as f:
          return json.load(f) #tähän palataan maanantaina
 
+def save_db(db):
+    with open("db.json", "w") as f:
+        return json.dump(db, f)
